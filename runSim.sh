2177d3f4a615d2cf8a9d1da16edfe852a38d2c60
sturla-freyr <stullif@gmail.com>
2022-11-12 22:11:33 +0000

    sim bara keyrður fyrir gildi sem gefa ca32KB cache

--- runSim.sh
+++ runSim.sh
@@ -3,7 +3,7 @@
 # Ef permission denied þarf að gera: chmod 755 runSim.sh
 # Annars bara keyra skrá með: bash runSim.sh > useData/result.txt
 
-E=8
+
 traces='mmulijk.trace ls.trace'
 for t in $traces
 do
@@ -11,10 +11,17 @@ do
   do
     for j in 3 4 5 6 7 8 9 10 11 12 13 14
     do
-        echo Results from s = $i E = $E b = $j t = traces/$t
-        echo
-        ./a.out -s $i -E $E -b $j -t traces/$t 
-        echo 
+    	S=$((2**i))
+    	B=$((2**j))
+    	E=$((32000/(S*B)))
+    	C=$(($S*$E*$B))
+    	if (($C < 33000 && $C > 31000))
+    	then
+          echo Results from s = $i E = $E b = $j t = traces/$t
+          echo
+          ./a.out -s $i -E $E -b $j -t traces/$t 
+          echo
+        fi 
     done
   done
 done
