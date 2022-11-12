cc05be9547708bae55ebb6d62333b66e446308f4
sturla-freyr <stullif@gmail.com>
2022-11-12 17:23:19 +0000

    bash scripta beturbætt og python scripta kláruð svo hægt er að sjá útkomu á keyrslu fyrir allar stærðir skyndiminna í allResults.txt

--- runSim.sh
+++ runSim.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 # Stranglega bannað að breyta línu 1 í þessu skjali
 # Ef permission denied þarf að gera: chmod 755 runSim.sh
-# Annars bara keyra skrá með: bash runSim.sh > result.txt
+# Annars bara keyra skrá með: bash runSim.sh > useData/result.txt
 
-sim=./a.out
-s='-s 1'
-b='-b 3'
-E='-E 8'
-yi='-t traces/yi.trace'
-
-for i in 1 2 3 4 5 6 7 8 9 10 11 12
+E=8
+traces='mmulijk.trace ls.trace'
+for t in $traces
 do
+  for i in 1 2 3 4 5 6 7 8 9 10 11 12
+  do
     for j in 3 4 5 6 7 8 9 10 11 12 13 14
     do
-        ./a.out -s $i -b $j $E $yi 
-        echo 
-        echo Results from s = $i, b = $j
+        echo Results from s = $i E = $E b = $j t = traces/$t
         echo
+        ./a.out -s $i -E $E -b $j -t traces/$t 
+        echo 
     done
+  done
 done
--- useData/processData.py
+++ useData/processData.py
@@ -1,7 +1,29 @@
 import pandas as pd
+import numpy as np
 
+s=[]
+E=[]
+b=[]
+t=[]
+hits=[]
+miss=[]
+evict=[]
 with open('/home/sturla/cCode/verk2/useData/result.txt') as f:
     for Line in f:
         if(Line != "\n"):
-            print(Line)
+            splitLine = np.asarray(Line.split())
+            if(len(splitLine) == 14):
+            	#print('s='+splitLine[4]+' E='+splitLine[7]+' b='+splitLine[10]+' file='+splitLine[13])
+            	s.append(splitLine[4])
+            	E.append(splitLine[7])
+            	b.append(splitLine[10])
+            	t.append(splitLine[13])
+            elif(len(splitLine) == 6):
+            	#print('hits='+splitLine[1]+' miss='+splitLine[3]+' e='+splitLine[5])
+            	hits.append(splitLine[1])
+            	miss.append(splitLine[3])
+            	evict.append(splitLine[5])
+            	
+df = pd.DataFrame({'s':s, 'E':E, 'b':b, 'traceFile':t, 'hits':hits, 'misses':miss, 'evictions':evict})
+df.to_csv('allResults.csv', index=False, encoding='utf-8')
 f.close()
